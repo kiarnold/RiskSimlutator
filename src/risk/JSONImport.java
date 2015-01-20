31eb9f6efc8512bf49a7b55424119ddb67a62188
kiarnold <keithiarnold@gmail.com>
2015-01-20 08:32:45 -0700

    Update JSONImport.java

--- src/risk/JSONImport.java
+++ src/risk/JSONImport.java
@@ -7,15 +7,27 @@ import org.json.parser.JSONParser;
 
 public class JSONImport {
 	private JSONObject jsonObject;
+	private board = new RiskBoard();
 	
 	public JSONImport(String fileName){
 		try {
 			Object obj = parser.parse(new FileReader(fileName));
 			jsonObject = (JSONObject) obj;
 			
+			board = parseNewBoard(jsonObject);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	/**
+	 * Parses out a fresh baord from a json object.
+	 * 
+	 * @param json	a jason object with the board setup information
+	 * @return	a risk board with the infor from the json file
+	 **/
+	private RiskBoard parseNewBoard(JSONObject json) {
+		return null;
+	}
 }
