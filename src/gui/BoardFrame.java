b55d3c99b0e7deb6e48a61621945f649d09c2ddc
Keith Arnold <keithiarnold@gmail.com>
2015-02-24 17:23:42 -0700

    Started building UI

---
+++ src/gui/BoardFrame.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Canvas;
+
+public class BoardFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BoardFrame frame = new BoardFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BoardFrame() {
+		setTitle("Simulation Screen");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(105, 105, 105));
+		contentPane.setBackground(new Color(248, 248, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		Canvas mainScreen = new Canvas();
+		mainScreen.setForeground(new Color(105, 105, 105));
+		mainScreen.setBackground(new Color(248, 248, 255));
+		contentPane.add(mainScreen, BorderLayout.CENTER);
+	}
+
+}
