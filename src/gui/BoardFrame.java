1a7ee017ad6890f0d8fafa4151669f9f70bb2771
Keith Arnold <keithiarnold@gmail.com>
2015-03-05 06:31:11 -0700

    Developed UI

--- src/gui/BoardFrame.java
+++ src/gui/BoardFrame.java
@@ -2,12 +2,19 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import risk.Territory;
+
 import java.awt.Color;
 import java.awt.Canvas;
+import java.util.List;
 
 public class BoardFrame extends JFrame {
 
@@ -35,11 +42,11 @@ public class BoardFrame extends JFrame {
 	public BoardFrame() {
 		setTitle("Simulation Screen");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(200, 75, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setForeground(new Color(105, 105, 105));
 		contentPane.setBackground(new Color(248, 248, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
@@ -47,7 +54,27 @@ public class BoardFrame extends JFrame {
 		mainScreen.setForeground(new Color(105, 105, 105));
 		mainScreen.setBackground(new Color(248, 248, 255));
 		contentPane.add(mainScreen, BorderLayout.CENTER);
+		
+	}
 	
+	private class BoardPanel extends JPanel {
+		   private void doDrawing(Graphics g) {
+
+		        Graphics2D g2d = (Graphics2D) g;
+		        
+		        int height = this.getHeight();
+		        int width = this.getWidth();
+		        
+		        g2d.drawOval(width/2, height/2, 5, 5);
+		        
+		    }
+		
+		   @Override
+		    public void paintComponent(Graphics g) {
+		        
+		        super.paintComponent(g);
+		        doDrawing(g);
+		    }
 		
 	}
 
