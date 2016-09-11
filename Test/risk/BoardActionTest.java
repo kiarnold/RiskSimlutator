913fadb5a636f503231c1d1db3242d3e272bb3a7
Keith Arnold <keithiarnold@gmail.com>
2016-09-10 20:29:12 -0700

    Created BoardActionTest
    Building out board action test cases. Working on pulling all actions in
    board class into the new action class.

---
+++ Test/risk/BoardActionTest.java
@@ -0,0 +1,21 @@
+package risk;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class BoardActionTest {
+	
+	@Before
+	public void setup() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	
+	
+}
